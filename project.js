4995e52898a08d9afd26c70d1bec9fabd1f45760
Accestol <veeti.pirttijarvi@gmail.com>
2023-09-11 18:05:04 +0300

    update

--- project.js
+++ project.js
@@ -16,3 +16,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
     });
 });
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('.menu');
+
+    menuToggle.addEventListener('click', () => {
+        menu.classList.toggle('open');
+    });
+
+    // Close the menu when a menu item is clicked
+    const menuItems = document.querySelectorAll('.menu ul li a');
+    menuItems.forEach((item) => {
+        item.addEventListener('click', () => {
+            menu.classList.remove('open');
+        });
+    });
+});
